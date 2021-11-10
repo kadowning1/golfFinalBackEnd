8134dd752911ca8cecd4fa6c53f5cdb931fd4bca
kadowning1 <kadowning1@gmail.com>
2021-11-10 11:31:50 -0500

    add

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 class UserFactory extends Factory
 {
@@ -14,11 +15,14 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $hashedPassword = Hash::make('string');
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            // 'email_verified_at' => now(),
+            // 'password' => Hash::check('plain-text', $hashedPassword),
+            'password' => $hashedPassword,
+
             'remember_token' => Str::random(10),
         ];
     }
