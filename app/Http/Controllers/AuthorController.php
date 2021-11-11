1e62e8ee289b92b3f738c7b5958be9ebf5b0d0a8
kadowning1 <kadowning1@gmail.com>
2021-11-11 14:44:12 -0500

    update project

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+use Illuminate\Http\Request;
+use App\Http\Resources\AuthorsResource;
+
+class AuthorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return AuthorsResource::collection(Author::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Author  $author
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Author $author)
+    {
+        return  new AuthorsResource($author);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Author  $author
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Author $author)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Author  $author
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Author $author)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Author  $author
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Author $author)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,13 +19,25 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
-Route::get('/user', function (Request $request) {
-    return $request->user()->toArray();
+Route::middleware('auth:api')->prefix('v1')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::apiResource('/authors', AuthorController::class);
+
+    // Route::get('/authors/{author}', [AuthorController::class, 'show']
+    // );
+    // Route::get('/authors', [AuthorController::class, 'index']
+    // );
 });
 
-Route::get('/test', function (Request $request) {
-    return 'Auth Passed';
-});
+// Route::get('/user', function (Request $request) {
+//     return $request->user()->toArray();
+// });
+
+// Route::get('/test', function (Request $request) {
+//     return 'Auth Passed';
+// });
 
 Route::get('/Jacob', function (Request $request) {
     return 'Jacob is very helpful';
